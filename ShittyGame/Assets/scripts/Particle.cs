0532c44ef9573e935a6e4959a0d87228ac6de37f
Yelyzaveta <afelia2005@gmail.com>
2020-05-12 20:05:35 +0300

    One more room

---
+++ ShittyGame/Assets/scripts/Particle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject explosion;
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+        }
+    }
+}
